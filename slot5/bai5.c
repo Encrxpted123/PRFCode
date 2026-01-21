e309adcc43cab32300942cfa5c53251f57a8a99e
Encrxpted123 <hoangluan.design289@gmail.com>
2026-01-21 09:35:38 +0700

    slot5

--- slot5/bai5.c
+++ slot5/bai5.c
@@ -4,13 +4,13 @@ int main(){
 	printf("Nhap N: ");
 	scanf("%d",&N);
 	if (N<100)
-		t=950*N;
+		t=N*950;
 	else if (N>=200)
-		t=1550*N;
+		t=950*100 + 1250 * 50 + 1350 * 50 + (N-200)*1550;
 	else if (N>=150)
-		t=1350*N;
+		t=950*100 + 1250 * 50 + 1350*(N-150);
 	else if (N>=100)
-		t=1250*N;
+		t=950*100 + 1250 * (N-100);
 	printf("So tien la: %d",t);
 	return 0;
 }
