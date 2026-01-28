8be2a57da5b53f4c5e2d52e6eb6094fbd08e7f60
Encrxpted123 <hoangluan.design289@gmail.com>
2026-01-28 08:58:34 +0700

    Workshop

--- Workshop/Workshop 1/bai5-tinh tien taxi.c
+++ Workshop/Workshop 1/bai5-tinh tien taxi.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 int main(){
-	float km, t,cash;
+	float soKm, soPhutCho,tongTien;
 	printf("Nhap quang duong di xe: ");
-	if (scanf("%f", &km)!=1 || km<=0){
+	if (scanf("%f", &soKm)!=1 || soKm<=0){
 		printf("Nhap sai!");
 		return 0;
 	}
 	printf("Nhap thoi gian cho: ");
-	scanf("%f",&t);
-	if (t>=5) //tinh gia tien t5heo thoi gian cho
-		cash = (t-5)*1;
+	scanf("%f",&soPhutCho);
+	if (soPhutCho>=5) //tinh gia tien t5heo thoi gian cho
+		tongTien = (soPhutCho-5)*1000;
 	else
-		cash = 0;
+		tongTien = 0;
 	
-	if (km<=0.5) //tinh gia tien 0-0.5km
-		cash=cash + 12;
-	else if (km>30) //tinh gia tien khi >30km
-		cash=cash + 12 + 15*29.5 + 12*(km-30);
+	if (soKm<=0.5) //tinh gia tien 0-0.5km
+		tongTien=tongTien + 12000;
+	else if (soKm>30) //tinh gia tien khi >30km
+		tongTien=tongTien + 12000 + 15000*29.5 + 12000*(soKm-30);
 	else //tinh gia tien 0.6-30km
-		cash=cash + 12 + 15*(km-0.5);
+		tongTien=tongTien + 12000 + 15000*(soKm-0.5);
 	printf("%-15s %-15s %-15s \n","So km","So phut cho","Tong tien");
-	if (km<=0.5)
-		printf("%-15.1f %-15.0f %-15.3f",km,t,cash);
-	if (km>0.5)
-		printf("%-15.0f %-15.0f %-15.3f",km,t,cash);
+	if (soKm<=0.5)
+		printf("%-15.1f %-15.0f %-15.3f",soKm,soPhutCho,tongTien);
+	if (soKm>0.5)
+		printf("%-15.0f %-15.0f %-15.3f",soKm,soPhutCho,tongTien);
 	return 0;
 }
--- Workshop/Workshop 1/bai6-tinh diem.c
+++ Workshop/Workshop 1/bai6-tinh diem.c
@@ -4,17 +4,17 @@ int main(){
 	char chu;
 	printf("Nhap diem chuyen can: ");
 	if (scanf("%f",&cc) != 1 || cc <0 || cc >10){
-		print("Nhap sai diem!");
+		printf("Nhap sai diem!");
 		return 0;
 	};
 	printf("Nhap diem giua ki: ");
 	if (scanf("%f",&gk) != 1 || gk <0 || gk >10){
-		print("Nhap sai diem!");
+		printf("Nhap sai diem!");
 		return 0;
 	};
 	printf("Nhap diem cuoi ki: ");
 	if (scanf("%f",&ck) != 1 || ck <0 || ck >10){
-		print("Nhap sai diem!");
+		printf("Nhap sai diem!");
 		return 0;
 	};
 	tong = cc*0.1 + gk*0.3 + ck*0.6;
@@ -29,9 +29,9 @@ int main(){
 	else
 		chu = 'F';
 	if (cc>=4 && gk>=4 && ck>=4 && chu!="F")
-		printf("Dat!");
+		printf("Ban da du dieu kien tot nghiep!");
 	else
-		printf("Khong dat!");
+		printf("ban khong du dieu kien tot nghiep!");
 		
 	
 }
